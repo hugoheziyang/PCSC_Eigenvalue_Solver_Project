b95ff2453f6fc6f763d1ffde80643d4a81f7ce60
ClemFoxtrot <clement.froidevaux@outlook.com>
2025-12-04 10:33:49 +0100

    Using same folder structure as the branch clement

--- html/search/files_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['solve_5fshifted_2ehpp_0',['solve_shifted.hpp',['../solve__shifted_8hpp.html',1,'']]]
-];
--- html/search/functions_7.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['matrix_0',['matrix',['../classMatrix.html#a12d35ef1824cd847868a8703035b0d5d',1,'Matrix::Matrix()=default'],['../classMatrix.html#ab0f3e6ea82c6fb02be3a02f6d5c6f26d',1,'Matrix::Matrix(const Eigen::MatrixBase&lt; Derived &gt; &amp;m)'],['../classMatrix.html#a5dbde436c14e8376532b0aa85df7c815',1,'Matrix::Matrix(const Eigen::SparseMatrix&lt; Scalar, Options, StorageIndex &gt; &amp;m)'],['../classMatrix.html#aa81b58bcc74ece79632881f9c0795ed6',1,'Matrix::Matrix(const std::vector&lt; Scalar &gt; &amp;data, std::size_t rows, std::size_t cols)'],['../classMatrix.html#abcf708e864ed3d9db2c3b317c5e529ab',1,'Matrix::Matrix(const Matrix &amp;other)']]]
-];
